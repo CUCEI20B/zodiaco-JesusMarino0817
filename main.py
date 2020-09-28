560f22726e595a2e128eddb07545ccc35dec015a
Jesus Marino <jesus.marino7468@alumnos.udg.mx>
2020-09-28 12:43:22 -0500

    prueba01

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ dia = int (input ())
 mes = int (input ())
 
 if (dia >= 21 and mes == 3) or (dia <= 20 and mes == 4):
-    print ('Aries')
+    print ('aries')
 if (dia >= 24 and mes == 9) or (dia <= 23 and mes == 10):
-    print ('Libra')
+    print ('libra')
 if (dia>=21 and mes==4) or (dia<=21 and mes==5):
-    print ('Tauro')
+    print ('tauro')
 if (dia>=24 and mes==10) or (dia<=22 and mes==11):
-    print ('Escorpio')
+    print ('escorpio')
 if (dia>=22 and mes==5) or (dia<=21 and mes==6):
-    print ('Geminis')
+    print ('geminis')
 if (dia>=23 and mes==11) or (dia<=21 and mes==12):
-    print ('Sagitario')
+    print ('sagitario')
 if (dia>=21 and mes==6) or (dia<=23 and mes==7):
-    print ('Cancer')
+    print ('cancer')
 if (dia>=22 and mes==12) or (dia<=20 and mes==1):
-    print ('Capricornio')
+    print ('capricornio')
 if (dia>=24 and mes==7) or (dia<=23 and mes==8):
-    print ('Leo')
+    print ('leo')
 if (dia>=21 and mes==1) or (dia<=19 and mes==2):
-    print ('Acuario')
+    print ('acuario')
 if (dia>=24 and mes==8) or (dia<=23 and mes==9):
-    print ('Virgo')
+    print ('virgo')
 if (dia>=20 and mes==2) or (dia<=20 and mes==3):
-    print ('Piscis')
+    print ('piscis')
